d01cbf8d3a28bf778652fde306b025e5c8465373
Jacob Kostecki <kostecki.jacob@gmail.com>
2020-07-16 19:55:37 +0200

    first run measurements and heartbeat

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -9,6 +9,9 @@
 #include <ArduinoJson.h>
 #include <RunningMedian.h>
 
+// Keep track of first run
+bool isFirstRun = true;
+
 // Voltage measurements
 int raw_analog_value = 0;
 float voltage = 0.0;
@@ -16,9 +19,12 @@ float voltage = 0.0;
 // Warning-LED Status
 bool warningLED = false;
 
-// Logging-interval time keeping
-unsigned long previousMillis = 0;
-const long logInterval = LOG_INTERVAL_HOURS * 3600000; // Convert hours (from logIntervalHours) to milliseconds
+// Intervals and time keeping
+unsigned long previousMillisLogInterval = 0;
+unsigned long previousMillisHeartbeat = 0;
+const long heartbeatInterval = HEARTBEAT_FREQUENCY_MINUTES * 60000; // Convert minutes (from HEARTBEAT_FREQUENCY_MINUTES) to milliseconds
+const long logInterval = LOG_INTERVAL_HOURS * 3600000; // Convert hours (from LOG_INTERVAL_HOURS) to milliseconds
+const long firstRunThreshold = FIRST_RUN_THRESHOLD_MINUTES * 60000; // Convert minutes (from FIRST_RUN_THRESHOLD_MINUTES) to milliseconds
 
 void setup(){
   Serial.begin(9600);
@@ -58,29 +64,37 @@ void setup(){
   ArduinoOTA.begin();
 }
 
-void loop(){
-    ArduinoOTA.handle();
+void sendHeartbeat() {
+  // Create JSON object
+  const size_t CAPACITY = JSON_OBJECT_SIZE(1);
+  StaticJsonDocument<CAPACITY> doc;
 
-    // Turn on red "warning led" if voltage is less than 20%
-    if (warningLED) {
-      digitalWrite(LED_PIN, HIGH);
-    }
+  // Populate JSON object
+  doc["batteryId"] = BATTERY_ID;
 
-    // Measure voltage every 6 (logInterval) hours
-    unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= logInterval) {
-      previousMillis = currentMillis;
-      createMeasurement();
-    }
+  // Serialized JSON is required for the POST request
+  String json;
+  serializeJson(doc, json);
+
+  // HTTPS Client setup
+  std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+  client->setInsecure();
+  HTTPClient https;
+
+  // Handle POST request
+  if (https.begin(*client, API_URL_HEARTBEAT)) {
+    https.POST(json);
+    https.end();
+  }
 }
 
 // Measure voltage of connected battery
-float createMeasurement() {
+float createMeasurement(bool postToAPI = false) {
   // Sample battery voltage 100 times and get median value
   RunningMedian samples = RunningMedian(100);
   for (int i = 0; i < 100; i++) {
     raw_analog_value = analogRead(VOLTAGE_INPUT_PIN);
-    voltage = raw_analog_value * 0.01243;  
+    voltage = raw_analog_value * 0.0124;  //0.01243
     samples.add(voltage);
   }
 
@@ -91,7 +105,9 @@ float createMeasurement() {
     warningLED = false;
   }
   
-  sendMeasurement(voltage);
+  if (postToAPI) {
+    sendMeasurement(voltage);
+  }
 }
 
 // Send measured voltage to API
@@ -114,8 +130,38 @@ void sendMeasurement(float measurement) {
   HTTPClient https;
 
   // Handle POST request
-  if (https.begin(*client, API_URL)) {
+  if (https.begin(*client, API_URL_MEASUREMENT)) {
     https.POST(json);
     https.end();
+  }
+}
+
+void loop(){
+  ArduinoOTA.handle();
+
+  // Time since boot
+  unsigned long currentMillis = millis();
+
+  // Send heartbeat to API
+  if (currentMillis - previousMillisHeartbeat >= heartbeatInterval) {
+    previousMillisHeartbeat = currentMillis;
+    sendHeartbeat();
+  }
+  
+  // Measure voltage on boot (with delay) for status LED
+  if (currentMillis > firstRunThreshold && isFirstRun) {
+    isFirstRun = false;
+    createMeasurement();
+  }
+
+  // Measure voltage every 6 (logInterval) hours
+  if (currentMillis - previousMillisLogInterval >= logInterval) {
+    previousMillisLogInterval = currentMillis;
+    createMeasurement(true);
+  }
+
+  // Turn on red "warning led" if voltage is less than 20%
+  if (warningLED) {
+    digitalWrite(LED_PIN, HIGH);
   }
 }
