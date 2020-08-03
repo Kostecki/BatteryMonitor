1aa543cc6ec0173191508440d02c1f2168d8037b
Jacob Kostecki <kostecki.jacob@gmail.com>
2020-08-03 11:00:07 +0200

    web

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -67,8 +67,11 @@ module.exports = {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
+        light: {
+          background: colors.grey.lighten4
+        },
         dark: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,92 +1,238 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
+  <v-row>
+    <v-col
+      v-for="battery in sortedBatteries"
+      :key="battery.name"
+      cols="12"
+      sm="8"
+      offset-sm="2"
+      offset-md="0"
+      md="6"
+      lg="5"
+      offset-lg="1"
+      offset-xl="0"
+      xl="3"
+      align="center"
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
+      <v-card class="px-4">
+        <v-row align="center" no-gutters>
+          <v-col cols="3" class="d-flex battery-percentage" :style="{color: setChargeColor(battery.latestVoltage) }">
+            <h1>
+              {{ calcBatteryCharge(battery.latestVoltage) }}%
+            </h1>
+          </v-col>
+          <v-col cols="9" class="card-main-content">
+            <v-card-title>
+              {{ battery.name }} ({{ battery.capacity }} Ah)
+            </v-card-title>
+            <v-card-subtitle>
+              <div>{{ `${battery.manufacturer}, ${battery.model}` }}</div>
+              <div>{{ `Voltage: ${battery.latestVoltage} Volt` }}</div>
+            </v-card-subtitle>
+          </v-col>
+        </v-row>
       </v-card>
-    </v-flex>
-  </v-layout>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import utils from '../utils/helperFunctions'
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  components: {},
+  data () {
+    return {
+      batteries: [
+        {
+          manufacturer: 'Sonnenschein',
+          createdAt: {
+            _seconds: 1591739962,
+            _nanoseconds: 779000000
+          },
+          capacity: 120,
+          model: 'GF 12 105 V',
+          notificationsSent: {
+            second: false,
+            first: false
+          },
+          latestVoltage: 12.3,
+          name: 'Cloud Raven',
+          updatedAt: {
+            _seconds: 1591739962,
+            _nanoseconds: 779000000
+          }
+        },
+        {
+          manufacturer: 'Sonnenschein',
+          createdAt: {
+            _seconds: 1591740044,
+            _nanoseconds: 696000000
+          },
+          capacity: 75,
+          model: 'GF 12 65 Y',
+          notificationsSent: {
+            second: false,
+            first: false
+          },
+          latestVoltage: 12.6,
+          name: 'Electric Chicken',
+          updatedAt: {
+            _seconds: 1591740044,
+            _nanoseconds: 696000000
+          }
+        },
+        {
+          notificationsSent: {
+            second: false,
+            first: false
+          },
+          model: 'Heavy Duty Extra II',
+          latestVoltage: 12,
+          name: 'Lightning Goose',
+          updatedAt: {
+            _seconds: 1591739853,
+            _nanoseconds: 539000000
+          },
+          manufacturer: 'Volvo',
+          createdAt: {
+            _seconds: 1591739853,
+            _nanoseconds: 539000000
+          },
+          capacity: 225
+        },
+        {
+          manufacturer: 'Tianneng',
+          createdAt: {
+            _seconds: 1592094072,
+            _nanoseconds: 422000000
+          },
+          capacity: 20,
+          notificationsSent: {
+            first: false,
+            second: false
+          },
+          model: '6-DZM-20',
+          latestVoltage: 12.8,
+          name: 'Energy Magpie',
+          updatedAt: {
+            _seconds: 1592094072,
+            _nanoseconds: 422000000
+          }
+        },
+        {
+          manufacturer: 'Varta',
+          createdAt: {
+            _seconds: 1591740021,
+            _nanoseconds: 369000000
+          },
+          capacity: 230,
+          model: 'LFD230',
+          notificationsSent: {
+            first: false,
+            second: false
+          },
+          latestVoltage: 12.5,
+          name: 'Power Pelican',
+          updatedAt: {
+            _seconds: 1591740021,
+            _nanoseconds: 369000000
+          }
+        },
+        {
+          updatedAt: {
+            _seconds: 1592297693,
+            _nanoseconds: 236000000
+          },
+          manufacturer: 'Sonnenschein',
+          createdAt: {
+            _seconds: 1591740086,
+            _nanoseconds: 386000000
+          },
+          capacity: 75,
+          notificationsSent: {
+            second: false,
+            first: false
+          },
+          model: 'GF 12 65 Y',
+          latestVoltage: 12.7,
+          name: 'Charge Dove'
+        },
+        {
+          model: 'Heavy Duty Extra II',
+          notificationsSent: {
+            first: false,
+            second: false
+          },
+          latestVoltage: 12.4,
+          name: 'Current Swallow',
+          updatedAt: {
+            _seconds: 1591739981,
+            _nanoseconds: 800000000
+          },
+          manufacturer: 'Volvo',
+          createdAt: {
+            _seconds: 1591739981,
+            _nanoseconds: 800000000
+          },
+          capacity: 225
+        },
+        {
+          createdAt: {
+            _seconds: 1591739892,
+            _nanoseconds: 390000000
+          },
+          capacity: 140,
+          model: 'K10',
+          notificationsSent: {
+            second: false,
+            first: false
+          },
+          latestVoltage: 12.1,
+          name: 'Storm Gobbler',
+          updatedAt: {
+            _seconds: 1591739892,
+            _nanoseconds: 390000000
+          },
+          manufacturer: 'Varta'
+        },
+        {
+          manufacturer: 'Powerfit',
+          createdAt: {
+            _seconds: 1591739939,
+            _nanoseconds: 831000000
+          },
+          capacity: 7.2,
+          model: 'S312/7 S',
+          notificationsSent: {
+            second: false,
+            first: false
+          },
+          latestVoltage: 12.2,
+          name: 'Thunder Duck',
+          updatedAt: {
+            _seconds: 1591739939,
+            _nanoseconds: 831000000
+          }
+        }
+      ]
+    }
+  },
+  computed: {
+    sortedBatteries () {
+      const batteries = this.batteries
+      return batteries.sort((a, b) => a.latestVoltage - b.latestVoltage)
+    }
+  },
+  methods: {
+    calcBatteryCharge: utils.calcBatteryCharge,
+    setChargeColor: utils.setChargeColor
   }
 }
 </script>
+
+<style>
+  .card-main-content {
+    text-align: left;
+  }
+</style>
