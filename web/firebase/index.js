b0eee878572069b1deda1572781c91fb9508550b
Jacob Kostecki <kostecki.jacob@gmail.com>
2020-08-10 13:51:34 +0200

    remove unnecessary keys

--- web/firebase/index.js
+++ web/firebase/index.js
@@ -6,10 +6,7 @@ const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DB_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: '',
-  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
-  appId: process.env.FIREBASE_APP_ID
+  projectId: process.env.FIREBASE_PROJECT_ID
 }
 
 export const db = !firebase.apps.length
