5dc0a7b6a695d1547d23bcca47c34e9a50b7a826
Jacob Kostecki <kostecki.jacob@gmail.com>
2020-08-12 10:50:35 +0200

    docker env vars test

--- web/firebase/index.js
+++ web/firebase/index.js
@@ -6,7 +6,7 @@ const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DB_URL,
-  projectId: 'batterymonitor-2b1c4'
+  projectId: process.env.FIREBASE_PROJECT_ID
 }
 
 console.log('this is index.js')
