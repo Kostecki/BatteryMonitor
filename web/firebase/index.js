66e4a4c7fe944acac5696409ba91351702206691
Jacob Kostecki <kostecki.jacob@gmail.com>
2020-08-12 10:29:05 +0200

    projectId?

--- web/firebase/index.js
+++ web/firebase/index.js
@@ -6,8 +6,11 @@ const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DB_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID
+  projectId: 'batterymonitor-2b1c4'
 }
+
+console.log('this is index.js')
+console.log(projectId)
 
 export const db = !firebase.apps.length
   ? firebase.initializeApp(config).firestore()
