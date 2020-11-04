f6828ff22de75c4f7306c54197f1be4274d8868f
Jacob Kostecki <kostecki.jacob@gmail.com>
2020-11-04 16:18:42 +0100

    fix base_config-file

--- sketch/base_config.h
+++ sketch/base_config.h
@@ -1,8 +1,3 @@
-// Device information
-const char* DEVICE_NAME = "Storm_Gobbler_Monitor";
-const char* BATTERY_ID = "-MIjJetQ_9DcHP_ZDB_K";
-const char* BATTERY_NAME = "Storm Gobbler";
-
 // API
 const char* API_URL_HEARTBEAT = "https://battery.israndom.win/api/heartbeat";
 const char* API_URL_MEASUREMENT = "https://battery.israndom.win/api/measurement";
@@ -11,9 +6,8 @@ const char* API_URL_MEASUREMENT = "https://battery.israndom.win/api/measurement"
 const char* WIFI_SSID = "Legacy";
 const char* WIFI_PASSWORD = "Malibu56";
 
-// OTA
+// OTA (OTA_NAME in board-config-file)
 int OTA_PORT = 8266;
-const char* OTA_NAME = "Storm_Gobbler_OTA";
 const char* OTA_PASSWORD = "met75afx"; 
 
 // Hardware setup
@@ -21,7 +15,6 @@ const int LED_PIN = D5;
 
 // ADC
 float ADC_RESOLUTION = 0.003;
-float VOLTAGE_DIVIDER_RATIO = 4.967; // Adjusted for resistor accuracy
 
 // Voltage of battery at 20%
 // For Warning-LED
